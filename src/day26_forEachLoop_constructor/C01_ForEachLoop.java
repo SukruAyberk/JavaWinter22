19159f52049ed92726467f6e5ac07b99c799f863
Ayberk Gürfidan <ayberk546@gmail.com>
2022-03-24 21:45:32 +0300

    second commit

---
+++ src/day26_forEachLoop_constructor/C01_ForEachLoop.java
@@ -0,0 +1,24 @@
+package day26_forEachLoop_constructor;
+
+public class C01_ForEachLoop {
+
+    public static void main(String[] args) {
+
+        int[] arr = {2, 4, 6, 8};
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        System.out.println();
+
+        // for-each loop çalıştırmak için hedef bir collection vermeliyiz
+
+        for (int i: arr) {
+            System.out.print(i + " ");
+        }
+
+
+
+    }
+}
