c0ee9b0e8462abdd2eec3a9182fa1c121c686fb0
Ayberk Gürfidan <ayberk546@gmail.com>
2022-04-11 19:20:01 +0300

    Exceptions

---
+++ src/day41_exceptions/C01_Exceptions.java
@@ -0,0 +1,23 @@
+package day41_exceptions;
+
+public class C01_Exceptions {
+
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 0;
+        int c = 0;
+
+        try {
+            c = a / b;
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+            System.out.println(e.toString());
+        }
+
+
+        System.out.println(c);
+
+    }
+
+}
