274473193e9fb0c01dc21616d44b44d4d5e20ec5
Ayberk Gürfidan <ayberk546@gmail.com>
2022-04-13 22:05:04 +0300

    Exceptions soru

---
+++ src/day42_exceptions/Soru1.java
@@ -0,0 +1,15 @@
+package day42_exceptions;
+
+public class Soru1 {
+
+    public static void main(String[] args) {
+
+        /*
+        Kullanicidan carpma yapmak icin bir String isteyin. Kullanicinin girdigi String sadece
+        sayilardan olusuyorsa sayiyi 2 ile carpip sonucu yazdirin.
+        Kullanici sayilardan olusmayan bir String girerse “Girdiginiz String sayiya cevrilemez”
+        yazdirin.
+         */
+
+    }
+}
---
+++ src/day42_exceptions/Soru2.java
@@ -0,0 +1,15 @@
+package day42_exceptions;
+
+public class Soru2 {
+
+    public static void main(String[] args) {
+
+        /*
+        String str[ ],Urun isimlerini tuttugumuz bir Array olsun. Kullanicidan istedigi urunun
+        sirasini isteyin ve istedigi urunu yazdirin.
+        Kullanici Array’de olan urun sayisindan buyuk bir sira no girerse “Girdiginiz sira urun
+        sayisindan buyuk” yazdirin
+        */
+
+    }
+}
