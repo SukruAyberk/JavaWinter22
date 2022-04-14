5cd675f8a2e6d5acdb7dedd7ba92e86b2d814721
Ayberk Gürfidan <ayberk546@gmail.com>
2022-04-14 19:22:42 +0300

    Interface

---
+++ src/day44_abstractClass_Interface/I03_Interface.java
@@ -0,0 +1,17 @@
+package day44_abstractClass_Interface;
+
+public interface I03_Interface {
+
+    /*
+    Full abstraction(tam soyutlama) oluşturmak istersek
+    abstract classları değil interface kullanırız
+
+    Interfacelerde sadece abstract methodlar bulunur
+
+    to do list yani burası burdan oluşturulacak methodlar belirleniyor
+    sonra o methodları çağırıp kullanıyoruz
+     */
+
+    public static final int SAYI = 20;
+
+}
