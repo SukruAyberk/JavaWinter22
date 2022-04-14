c6c9c839dd6fe38720491fea148b966f1972ad1c
Ayberk Gürfidan <ayberk546@gmail.com>
2022-04-14 18:35:29 +0300

    Abstract

---
+++ src/day44_abstractClass_Interface/C02_Toyota.java
@@ -0,0 +1,15 @@
+package day44_abstractClass_Interface;
+
+public class C02_Toyota extends C01_AbstractClass {
+
+
+    @Override
+    public void motor() {
+
+    }
+
+    @Override
+    public void vites() {
+
+    }
+}
